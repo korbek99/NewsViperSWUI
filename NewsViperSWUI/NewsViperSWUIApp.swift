d0d6ef509cd75b609ce847a561c1b9619df9a380
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-02-02 02:29:18 -0300

    Initial Commit

---
+++ NewsViperSWUI/NewsViperSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsViperSWUIApp.swift
+//  NewsViperSWUI
+//
+//  Created by Jose Preatorian on 02-02-26.
+//
+
+import SwiftUI
+
+@main
+struct NewsViperSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
